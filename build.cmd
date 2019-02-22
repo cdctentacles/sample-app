3ebbdd50418cc604cb852f84ce4d43d993b0d5ff
Ashish Negi <asnegi@microsoft.com>
2019-02-22 10:38:13 +0530

    create scd apps for win10-x64

--- build.cmd
+++ build.cmd
@@ -1,7 +1,7 @@
 pushd VotingData
-dotnet publish -o ..\VotingApp\VotingDataPkg\Code\
+dotnet publish -o ..\VotingApp\VotingDataPkg\Code\ -r win10-x64
 popd
 
 pushd VotingWeb
-dotnet publish -o ..\VotingApp\VotingWebPkg\Code\
+dotnet publish -o ..\VotingApp\VotingWebPkg\Code\ -r win10-x64
 popd
